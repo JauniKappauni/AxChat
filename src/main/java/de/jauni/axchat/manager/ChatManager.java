c9d81cd4e7796f2d292bc174d3d62e4a2170a4a3
JauniKappauni <social.jauni@gmail.com>
2026-01-23 12:33:19 +0100

    fix: replaced static values from subscriber object with custom of config.yml

--- src/main/java/de/jauni/axchat/manager/ChatManager.java
+++ src/main/java/de/jauni/axchat/manager/ChatManager.java
@@ -1,10 +1,15 @@
 package de.jauni.axchat.manager;
 
+import de.jauni.axchat.AxChat;
 import org.bukkit.Bukkit;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPubSub;
 
 public class ChatManager {
+    AxChat reference;
+    public ChatManager(AxChat reference){
+        this.reference = reference;
+    }
     Jedis publisher;
 
     public ChatManager(String host, int port) {
@@ -19,7 +24,7 @@ public class ChatManager {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                try (Jedis subscriber = new Jedis("localhost", 6379)) {
+                try (Jedis subscriber = new Jedis(reference.getConfig().getString("redis.host"), reference.getConfig().getInt("redis.port"))) {
                     subscriber.subscribe(new JedisPubSub() {
                         @Override
                         public void onMessage(String ch, String message) {
