102e2db626291af8726d1b7e9580c1c88d73f3f8
JauniKappauni <social.jauni@gmail.com>
2026-01-25 12:45:16 +0100

    feat: add chat filter function

--- src/main/java/de/jauni/axchat/listener/ChatListener.java
+++ src/main/java/de/jauni/axchat/listener/ChatListener.java
@@ -1,11 +1,13 @@
 package de.jauni.axchat.listener;
 
 import de.jauni.axchat.AxChat;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
 
 public class ChatListener implements Listener {
+    String[] forbiddenWords = {"badword1", "badword2", "badlink"};
     AxChat reference;
 
     public ChatListener(AxChat reference) {
@@ -14,8 +16,22 @@ public class ChatListener implements Listener {
 
     @EventHandler
     public void onChatMessage(PlayerChatEvent e) {
-        String msg = e.getPlayer().getName() + " " + ":" + " " + e.getMessage();
-        reference.getChatManager().publishMessage("global_chat", msg);
-        e.setCancelled(true);
+        String message = e.getMessage();
+        Player p = e.getPlayer();
+        boolean containsForbidden = false;
+        for (String word : forbiddenWords) {
+            if (message.toLowerCase().contains(word)) {
+                containsForbidden = true;
+                break;
+            }
+        }
+        if (containsForbidden) {
+            p.sendMessage("Deine Nachricht wurde blockiert!");
+            e.setCancelled(true);
+        } else {
+            String msg = p.getName() + " " + ":" + " " + message;
+            reference.getChatManager().publishMessage("global_chat", msg);
+            e.setCancelled(true);
+        }
     }
 }
